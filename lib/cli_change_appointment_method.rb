80031c7bafcc3c1db541f6cb8a9b711755d79507
augustoibarrola <augusto.t.ibarrola@gmail.com>
2020-08-20 16:11:38 -0400

    updated appointment functionality

--- lib/cli_change_appointment_method.rb
+++ lib/cli_change_appointment_method.rb
@@ -5,14 +5,14 @@ def change_appointment
 
     prompt.ask("#{client_appointments.client.name}, we found your appointment on #{client_appointments.date} at #{client_appointments.tattoo_shop.name}")
     options = ["Change Appointment", "Delete Appointment"]
-    selected_option = prompt.select("Would you like to change or delete?", options)
+    selected_option = prompt.select("Would you like to change or delete this appointment?", options)
     if selected_option == options[0]
         prompt.ask("Great. Please answer a few questions:")
         prompt.collect do
         new_date = key(:date).ask("What day should we set your new appointment?", required: true)
         new_time = key(:time).ask("Got it. What time?", required: true)
         new_description = key(:description).ask("Please verify the work you would like done by providing a description", required: true)
-        client_appointments[date: new_date, time: new_time, description: new_description]
+        client_appointments.update(date: new_date, time: new_time, description: new_description)
         prompt.ask("#{client_appointments.client.name}, we will now see you on #{new_date} at #{new_time} to get your #{new_description}")
         end
     end
