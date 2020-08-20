c8e197df07b63f0bde2caf1600a7d91dd20e0623
augustoibarrola <augusto.t.ibarrola@gmail.com>
2020-08-20 15:20:34 -0400

    working further on change_appointment

--- lib/cli_change_appointment_method.rb
+++ lib/cli_change_appointment_method.rb
@@ -6,8 +6,15 @@ def change_appointment
     #     appt.client.id == Client.current_client 
     client_appointments = Appointment.all.find_by client: Client.current_client 
     # client_appointments.client_id.name 
-    prompt.ask("We found your appointment on #{client_appointments.date}")
-    binding.pry   
+    prompt.ask("#{client_appointments.client.name}, we found your appointment on #{client_appointments.date}")
+
+    prompt.collect do
+        new_date = key(:date).ask("What day should we set your new appointment?", required: true)
+        new_time = key(:time).ask("Got it. What time?", required: true)
+        new_description = key(:description).ask("Please verify the work you would like done by providing a description", required: true)
+        prompt.ask("#{client_appointments.client.name}, we will now see you on #{new_date} at #{new_time} to get your #{new_description}")
+    end
+     
               
     #     if appt.client.id == client_id 
     #        appt.client
